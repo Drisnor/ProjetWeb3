2d1afa26e6f1c92dbef17fbf3ea8d11323ce0a74
Drisnor <floflu19@gmail.com>
2019-04-19 22:35:04 +0200

    Requête UPDATE parcs OK

--- PHP/updateJeux.php
+++ PHP/updateJeux.php
@@ -4,16 +4,17 @@
 
 	/* Récupération des données JSON */
     if( isset($_GET['id']) && isset($_GET['superficie']) && isset($_GET['note'])) {  // si les données sont bien définies
-		$id = $_GET['id'];
-		$superficie = $_GET['superficie'];
-		$note = $_GET['note'];
 
 		/* Requête UPDATE */
-		$sql = "UPDATE jeux SET superficie=?, note=? WHERE id=?";
-		$stmt= $dpo->prepare($sql);
-		$stmt->execute([$superficie, $note, $id]);
+		$sql = "UPDATE jeux SET superficie=:sup, note=:note WHERE id=:id";
+		$stmt = $cnx->prepare($sql);
+		$stmt->bindParam(':id', $_GET['id'], PDO::PARAM_STR);
+		$stmt->bindParam(':sup', $_GET['superficie'], PDO::PARAM_STR);
+		$stmt->bindParam(':note', $_GET['note'], PDO::PARAM_STR);
+
+		$stmt->execute();
 
     } else {  // ERR
-
+    	echo 'ERREUR UPDATE';
     }
 ?>
