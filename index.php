f44d3136062c7713c4ed7345b7ce5f7ec5a193d2
Zoryx <florian81000@hotmail.fr>
2019-05-02 15:56:49 +0200

    Scroll Top Button Dimensions Statistique modif

--- index.php
+++ index.php
@@ -9,6 +9,10 @@
         <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
         <script type="text/javascript" src="includes/lrm-graphhopper-1.2.0.js"></script>
 
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+		<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+		
         <script type="text/javascript" src="includes/jquery.min.js"></script>
         <script type="text/javascript" src="includes/leaflet.geometryutil.js"></script>
         <script type="text/javascript" src="includes/Chart.min.js"></script>
@@ -34,13 +38,38 @@
                 display: inline-block;
             }
         </style>
+		
+		<script>
+			$(document).ready(function(){
+				$(window).scroll(function () {
+						if ($(this).scrollTop() > 50) {
+							$('#back-to-top').fadeIn();
+						} else {
+							$('#back-to-top').fadeOut();
+						}
+					});
+					// scroll body to 0px on click
+					$('#back-to-top').click(function () {
+						$('#back-to-top').tooltip('hide');
+						$('body,html').animate({
+							scrollTop: 0
+						}, 800);
+						return false;
+					});
+        
+					$('#back-to-top').tooltip('show');
+
+			});
+		</script>
     </head>
 
     <body>
         <!-- Le conteneur de notre carte (avec une contrainte CSS pour la taille) -->
-        <div id="macarte" style="width: 80%; height: 600px;"></div>
-        
-        <canvas id="pie-chart" width="800" height="450"></canvas>
+        <div id="macarte" style="width: 90%; height: 600px;"></div>
+        <!-- Button go top -->
+		<a id="back-to-top" href="#" class="btn btn-primary btn-lg back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><span class="glyphicon glyphicon-chevron-up"></span></a>
+	    <!-- Canvas affichage Stats -->
+        <canvas id="pie-chart" width="400" height="50"></canvas>
         <!-- Affichage et traitement des données de la carte -->
         <script type="text/javascript">
             /*******************************************************************************************************/
